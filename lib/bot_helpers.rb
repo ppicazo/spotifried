dc242c5aeda98eb386c202c7a73f1794b37f06f4
Paul Picazo <ppicazo@gmail.com>
2016-04-16 14:44:13 -0700

    * refactoring helper methods

--- lib/bot_helpers.rb
+++ lib/bot_helpers.rb
@@ -1,5 +1,6 @@
 module Spotifried
   class Helpers
+    SPOTIFY_TRACK_REGEX = /(?:(?:http(?:s?):\/\/(?:open|play)\.spotify\.com\/track\/)|(?:spotify:track:))(\w*)/
 
     def self.message_for_bot?(data, id)
       data.text.include? "<@#{id}>"
@@ -10,16 +11,15 @@ module Spotifried
     end
 
     def self.text_has_spotify_track?(text)
-      (text.include? "https://open.spotify.com/track/") || (text.include? "https://play.spotify.com/track/") || (text.include? "spotify:track:")
+      extract_spotify_tracks(text).count > 0
     end
 
     def self.spotify_track(data)
-      extract_spotify_track(data.text)
+      extract_spotify_tracks(data.text).first
     end
 
-    def self.extract_spotify_track(text)
-      tracks = /(?:(?:http(?:s?):\/\/(?:open|play)\.spotify\.com\/track\/)|(?:spotify:track:))(\w*)/.match(text)
-      tracks.is_a?(MatchData) && tracks.size > 1 ? tracks[1] : nil
+    def self.extract_spotify_tracks(text)
+      text.is_a?(String) ? text.scan(SPOTIFY_TRACK_REGEX).map { |m| m.first } : []
     end
   end
 end
--- spec/lib/bot_helpers_spec.rb
+++ spec/lib/bot_helpers_spec.rb
@@ -51,7 +51,7 @@ describe Spotifried::Helpers do
 
   describe "#spotify_track" do
     it "should call text_has_spotify_track? and return the result" do
-      expect(Spotifried::Helpers).to receive(:extract_spotify_track).with("abc").and_return("xyz")
+      expect(Spotifried::Helpers).to receive(:extract_spotify_tracks).with("abc").and_return(["xyz"])
       expect(Spotifried::Helpers.spotify_track(Hashie::Mash.new({ text: "abc" }))).to eq("xyz")
     end
   end
@@ -61,33 +61,37 @@ describe Spotifried::Helpers do
       @track_id = "6BivCuyKJtgLa9ooFsvUoZ"
     end
 
-    it "should return true on play urls" do
-      expect(Spotifried::Helpers.extract_spotify_track("hi https://play.spotify.com/track/#{@track_id} there")).to eq(@track_id)
-      expect(Spotifried::Helpers.extract_spotify_track("hi http://play.spotify.com/track/#{@track_id} there")).to eq(@track_id)
+    it "should return track id on play urls" do
+      expect(Spotifried::Helpers.extract_spotify_tracks("hi https://play.spotify.com/track/#{@track_id} there")).to eq([@track_id])
+      expect(Spotifried::Helpers.extract_spotify_tracks("hi http://play.spotify.com/track/#{@track_id} there")).to eq([@track_id])
     end
 
-    it "should return true on open urls" do
-      expect(Spotifried::Helpers.extract_spotify_track("hi https://open.spotify.com/track/#{@track_id} there")).to eq(@track_id)
+    it "should return track id on open urls" do
+      expect(Spotifried::Helpers.extract_spotify_tracks("hi https://open.spotify.com/track/#{@track_id} there")).to eq([@track_id])
     end
 
-    it "should return true on spotify uri" do
-      expect(Spotifried::Helpers.extract_spotify_track("spotify:track:#{@track_id} there")).to eq(@track_id)
+    it "should return track id on spotify uri" do
+      expect(Spotifried::Helpers.extract_spotify_tracks("spotify:track:#{@track_id} there")).to eq([@track_id])
     end
 
-    it "should return false on google.com links" do
-      expect(Spotifried::Helpers.extract_spotify_track("hi https://www.google.com/track/#{@track_id} there")).to be_falsey
+    it "should return track id on spotify uri more than one" do
+      expect(Spotifried::Helpers.extract_spotify_tracks("spotify:track:#{@track_id} spotify:track:second  there")).to eq([@track_id, "second"])
+    end
+
+    it "should return track id on google.com links" do
+      expect(Spotifried::Helpers.extract_spotify_tracks("hi https://www.google.com/track/#{@track_id} there")).to eq([])
     end
 
     it "should return nil on strings without tracks" do
-      expect(Spotifried::Helpers.extract_spotify_track("hi there")).to be_falsey
+      expect(Spotifried::Helpers.extract_spotify_tracks("hi there")).to eq([])
     end
 
     it "should return nil on empty strings" do
-      expect(Spotifried::Helpers.extract_spotify_track("")).to be_falsey
+      expect(Spotifried::Helpers.extract_spotify_tracks("")).to eq([])
     end
 
     it "should return nil on nil" do
-      expect(Spotifried::Helpers.extract_spotify_track(nil)).to be_falsey
+      expect(Spotifried::Helpers.extract_spotify_tracks(nil)).to eq([])
     end
   end
 end
